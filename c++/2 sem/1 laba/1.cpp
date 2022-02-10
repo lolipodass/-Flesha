dcc382163ec539148fc12618871de44756964aa8
lolipodass <andremae10@gmail.com>
2022-02-10 22:28:20 +0300

    full project

--- c++/2 sem/1 laba/1.cpp
+++ c++/2 sem/1 laba/1.cpp
@@ -3,15 +3,14 @@
 // 	 ввод элементов структуры с клавиатуры;
 // 	 вывод элементов структуры в консольное окно;
 // 	TODO удаление заданной структурированной переменной;
-// 	TODO поиск информации;
-// 	TODO запись информации в файл;
-// 	TODO чтение данных из файла.
+// 	поиск информации;
+// 	запись информации в файл;
+// 	чтение данных из файла.
 // !Горожанин. Ф.И.О., дата рождения, адрес, пол (м, ж). Реализовать выборку по году рождения.
 
 #include <iostream>
 #include <string>
 #include <windows.h>
-#include <vector>
 #include <fstream>
 
 using namespace std;
@@ -19,16 +18,19 @@ using namespace std;
 struct citizen
 {
     string name;
-    unsigned int age;
+    unsigned int age = 0;
     string address;
     bool sex;
+    bool uses = 0;
 };
 
 void structWrite(citizen *);
 void structRead(citizen *);
-vector<citizen> findStuct(citizen *, short, int);
+void findStuct(citizen *, short, int);
 void choose(citizen *, short, short);
-void FileRead(citizen *, short);
+void FileRead(citizen *, short, short);
+void decode(citizen *, string, short);
+string encode(citizen *);
 
 int main()
 {
@@ -43,7 +45,7 @@ int main()
     short choice;
     while (1)
     {
-        cout << "1 input struct\n2 console log struct\n3 find element with date\n4 read from file\n-1 break\n";
+        cout << "1 input struct\n2 console log struct\n3 find element with date\n4 read from file\n5 write to file\n6 delete element\n-1 break\n";
 
         cin >> choice;
         if (choice == -1)
@@ -71,36 +73,75 @@ void choose(citizen *arr, short choice, short size)
         break;
     case 3:
     {
-        vector<citizen> buff;
         cout << "what year u need?";
         short find;
         cin >> find;
-        buff = findStuct(arr, size, find);
-        for (short i = 0; i < buff.size(); i++)
-        {
-            structRead(&arr[i]);
-        }
+        findStuct(arr, size, find);
     }
     break;
     case 4:
-        FileRead(arr, 0);
+        FileRead(arr, 0, size);
         break;
+    case 5:
+    {
+        ofstream file;
+        cout << "\nType file name(with ext)";
+        char fileName[50];
+        do
+        {
+            cin >> fileName;
+            file.open(fileName);
+            if (!file.is_open())
+            {
+                cout << "with " << fileName << " some problem, choose different ";
+                continue;
+            }
+            break;
+        } while (1);
+
+        for (short i = 0; i < size; i++)
+        {
+            // string line = encode(&arr[i]);
+
+            file << "NAME:" << arr[i].name << " AGE:" << arr[i].age << " STREET: " << arr[i].address << " GENDER: ";
+            if (arr[i].sex == 1)
+                file << "m\n";
+            else
+                file << "w\n";
+        }
+        file.close();
+    }
+    break;
+    case 6:
+    {
+        short pos;
+        do
+        {
+            cout << "what element u want to delete? ";
+            cin >> pos;
+            if (pos > size)
+            {
+                cout << "\n wrong number, try again ";
+                continue;
+            }
+            break;
+        } while (1);
+        arr[pos].uses = 0;
+    }
     default:
         break;
     }
 }
 
-vector<citizen> findStuct(citizen *arr, short size, int date)
+void findStuct(citizen *arr, short size, int date)
 {
-    vector<citizen> buff;
     for (short i = 0; i < size; i++)
     {
-        if (arr[i].age == date)
+        if (arr[i].age == date && arr[i].uses == 1)
         {
-            buff.push_back(arr[i]);
+            structRead(&arr[i]);
         }
     }
-    return buff;
 }
 
 void structWrite(citizen *cit)
@@ -116,21 +157,30 @@ void structWrite(citizen *cit)
     getline(cin, cit->address);
     cout << "gender(m/w)\n";
 
-ret:
-    cin >> buf;
-    if (buf == 'm')
-        cit->sex = 1;
-    if (buf == 'w')
-        cit->sex = 0;
-    if (buf != 'm' && buf != 'w')
+    do
     {
-        cout << "wrong input,try again: ";
-        goto ret;
-    }
+        cin >> buf;
+        if (buf != 'm' && buf != 'w')
+        {
+            cout << "wrong input,try again: ";
+            continue;
+        }
+        if (buf == 'm')
+            cit->sex = 1;
+        if (buf == 'w')
+            cit->sex = 0;
+        break;
+    } while (1);
+    cit->uses = 1;
 }
 
 void structRead(citizen *cit)
 {
+    if (!cit->uses)
+    {
+        return;
+    }
+
     cout << "Full name: " << cit->name << ",Age: " << cit->age << ",address: " << cit->address << ", gender: ";
     if (cit->sex)
         cout << "Male\n";
@@ -138,8 +188,10 @@ void structRead(citizen *cit)
         cout << "Female\n";
 }
 
-void FileRead(citizen *cit, short start)
+void FileRead(citizen *cit, short start, short size)
 {
+
+    // *file check
     cout << "\nType file name(with ext)";
     ifstream fin;
     char fileName[50];
@@ -152,28 +204,57 @@ back:
         goto back;
     }
 
-    string line;
-    getline(fin, line);
-    cout << line;
+    short kol;
+    cout << "how many strings read? ";
+    do
+    {
+        if (kol > size)
+        {
+            cout << "array smaller, try again ";
+            continue;
+        }
+        break;
+    } while (1);
 
+    cin >> kol;
+    for (short i = 0; i < kol; i++)
+    {
+        string line;
+        getline(fin, line);
+        decode(cit, line, i + start);
+    }
+    fin.close();
+}
+
+void decode(citizen *cit, string line, short pos)
+{
+    // *string check
     short startName = line.find("NAME:");
     short startAge = line.find("AGE:", startName);
     short startStreet = line.find("STREET:", startAge);
     short startGender = line.find("GENDER:", startStreet);
-    short npos = string::npos;
+    short npos = static_cast<short>(string::npos);
     if (startName == npos && startAge == npos && startStreet == npos && startGender == npos)
     {
         cout << "\nbad file, try again: ";
-        goto back;
+        return;
     };
 
     startName += 5;
-    cit[start].name = line.substr(startName, startAge - startName);
-    cit[start].age = stoi(line.substr(startAge + 4, startStreet - startAge - 4));
-    cit[start].address = line.substr(startStreet + 7, startGender - startStreet - 7);
+    cit[pos].name = line.substr(startName, startAge - startName);
+    cit[pos].age = stoi(line.substr(startAge + 4, startStreet - startAge - 4));
+    cit[pos].address = line.substr(startStreet + 7, startGender - startStreet - 7);
 
     if (line.substr(startGender + 7, line.size() - startGender - 7) == "m")
-        cit[start].sex = 1;
+        cit[pos].sex = 1;
     if (line.substr(startGender + 7, line.size() - startGender - 7) == "w")
-        cit[start].sex = 0;
+        cit[pos].sex = 0;
+    cit[pos].uses = 1;
+}
+
+string encode(citizen *cit)
+{
+    string line;
+    line = "NAME:" + cit->name + " AGE:" + static_cast<char>(cit->age) + " STREET: " + cit->address + " GENDER: " + static_cast<char>(cit->sex);
+    return line;
 }
