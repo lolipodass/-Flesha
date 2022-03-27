a5dbf541190a358ef4180f552b4849f7112661cf
lolipodass <andremae10@gmail.com>
2022-03-27 14:55:41 +0300

    mini

--- c++/2 sem/3 lab/1.cpp
+++ c++/2 sem/3 lab/1.cpp
@@ -16,49 +16,89 @@ union date
     bytefield bit;
 };
 
+enum spec
+{
+    ISIT = 1,
+    POIBMS,
+    DEVI,
+    POIT
+};
+
 struct account
 {
     string name;
-    string spec;
+    spec spec;
     int division;
     int position;
     date date;
 };
 
+void fullStruct(account *);
+void ShowStruct(account *);
+
 int main()
 {
+    int size;
+    cout << "size array";
+    account *arr = new account[size];
+
+    for (int i = 0; i < size; i++)
+    {
+
+        fullStruct(arr);
+        ShowStruct(arr);
+    }
+}
+
+void fullStruct(account *acc)
+{
+    int buff;
+    cout << "имя: ";
+    cin >> acc->name;
+    cout << "специальность:\t1 исит\t2 поибмс\t 3 деви\t 4 поит";
+    cin >> buff;
+
+    acc->spec = static_cast<spec>(buff);
+    cout << "division: ";
+    cin >> acc->division;
+    cout << "position: ";
+    cin >> acc->position;
+
+    cout << "date, year: ";
+    cin >> buff;
+    acc->date.bit.year = buff;
+    cout << "date, mouth: ";
+    cin >> buff;
+    acc->date.bit.month = buff;
+    cout << "date, day: ";
+    cin >> buff;
+    acc->date.bit.day = buff;
+}
+
+void ShowStruct(account *acc)
+{
+    cout << "\nName: " << acc->name << "\nSpecial: ";
+    switch (acc->spec)
+    {
+    case ISIT:
+        cout << "ISIT";
+        break;
+    case POIBMS:
+        cout << "POIBMS";
+        break;
+    case DEVI:
+        cout << "ISIT";
+        break;
+    case POIT:
+        cout << "POIT";
+        break;
+    default:
+        break;
+    }
+    cout << "\nDivision: " << acc->division << "\nPosition: " << acc->position << "\n";
+    cout << "Date:\n\tYear\t" << acc->date.bit.year << "\n\tMouth\t" << acc->date.bit.month << "\n\tDay\t" << acc->date.bit.day;
 }
 
 // *Отдел кадров.
 //! Ф.И.О. работника, образование, специальность, подразделение, должность, оклад, дата поступления на предприятие.
 // !Выбор по стражу работы. Даты реализовать с помощью битового поля, должность - с помощью перечисления.
-
-void structWrite(citizen *cit)
-{
-    cin.ignore(10, '\n');
-    char buf;
-    cout << "Full name\n";
-    getline(cin, cit->name);
-    cout << "Age\n";
-    cin >> cit->age;
-    cout << "address\n";
-    cin.ignore(10, '\n');
-    getline(cin, cit->address);
-    cout << "gender(m/w)\n";
-
-    do
-    {
-        cin >> buf;
-        if (buf != 'm' && buf != 'w')
-        {
-            cout << "wrong input,try again: ";
-            continue;
-        }
-        if (buf == 'm')
-            cit->sex = 1;
-        if (buf == 'w')
-            cit->sex = 0;
-        break;
-    } while (1);
-    cit->uses = 1;
-}
