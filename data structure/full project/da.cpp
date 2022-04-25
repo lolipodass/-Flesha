f3ca5e42efd74d7dd75f746c5ed0d24270e3f82f
lolipodass <56706038+lolipodass@users.noreply.github.com>
2022-04-25 13:02:02 +0300

    full data stuct

--- data structure/full project/da.cpp
+++ data structure/full project/da.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+using std::cout,std::cin;
 
+const int N = 17;
+//интерполяционный поиск
+int InterpolSearch(int A[], int key)
+{
+    int mid, left = 0, right = N - 1;
+    while (A[left] <= key && A[right] >= key)
+    {
+        mid = left + ((key - A[left]) * (right - left)) / (A[right] - A[left]);
+        if (A[mid] < key)
+            left = mid + 1;
+        else if (A[mid] > key)
+            right = mid - 1;
+        else
+            return mid;
+    }
+    if (A[left] == key)
+        return left;
+    else
+        return -1;
+}
+//главная функция
 int main()
 {
-    const int num = 10;
-    const int* p = &num;
-    *p = 20;
-
-
-    return 0;
+    int i, key;
+    int A[N] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59};
+    cout << "Искомый элемент > ";
+    cin >> key; //ввод ключа
+    cout << "Исходный массив: ";
+    for (i = 0; i < N; i++)
+        cout << A[i] << " "; //вывод массива
+    if (InterpolSearch(A, key) == -1)
+        cout << "\nЭлемент не найден";
+    else
+        cout << "\nНомер элемента: " << InterpolSearch(A, key) + 1;
 }
--- data structure/full project/full.cpp
+++ data structure/full project/full.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-short arraySize;
 
 struct contract
 {
@@ -14,16 +13,21 @@ struct contract
     short workExp;
 };
 
-void ReadFromFile(contract *);
+
+void ReadFromFile(contract *,size_t);
 void FullContract(contract *);
-void WriteToFile(contract *, int);
-contract *Choosing(contract *, short);
-void ShowStruct(contract *, int);
-contract *deleteElements(contract *, int, int, int);
-contract *copyArray(contract *, int);
+void WriteToFile(contract *, size_t);
+contract *Choosing(contract *,size_t, short);
+void ShowStruct(contract *, size_t);
+contract *deleteElements(contract *, size_t, int, int);
+contract *copyArray(contract *, size_t);
+void sorting(contract*,size_t, int ,bool );
+void findValue(contract *,size_t);
+int InterpolSearch(contract *,size_t,int);
+int binarySeacrh(contract *, size_t, int);
 
 template <typename T>
-bool sorting(T val1, T val2, bool sorting)
+bool compare(T val1, T val2, bool sorting)
 {
     if (val1 > val2 && sorting)
         return 1;
@@ -36,24 +40,25 @@ int main()
 {
 
     cout << "Array size: ";
-    cin >> arraySize;
-    contract *con = new contract[arraySize];
+    size_t length;
+    cin >> length;
+    contract *arr = new contract[length];
 
     short value;
 
     while (1)
     {
-        cout << "\n1 вручную\n2 чтение из файла\n3 запись в файл\n4 удаление записей\n5 показ структуры\n6 сортировка\n7 фильтрация\n";
+        cout << "\n1 вручную\n2 чтение из файла\n3 запись в файл\n4 удаление записей\n5 показ структуры\n6 сортировка\n7 фильтрация\n8 поиск\n";
         cin >> value;
-        if (!(value == 1 || value == 2 || value == 3 || value == 4 || value == 5 || value == 6 || value == 7))
+        if (!(value == 1 || value == 2 || value == 3 || value == 4 || value == 5 || value == 6 || value == 7||value==8))
         {
             break;
         }
-        con = Choosing(con, value);
+        arr = Choosing(arr,length, value);
     }
 }
 
-contract *Choosing(contract *arr, short choice)
+contract *Choosing(contract *arr,size_t length, short choice)
 {
 
     switch (choice)
@@ -63,7 +68,7 @@ contract *Choosing(contract *arr, short choice)
         cout << "сколько элементов заполнить?(0=all)";
         int amount;
         cin >> amount;
-        (amount > arraySize) || (amount == 0) ? amount = arraySize : 0;
+        (amount > length) || (amount == 0) ? amount = length : 0;
         for (short i = 0; i < amount; i++)
         {
             FullContract(&arr[i]);
@@ -72,11 +77,11 @@ contract *Choosing(contract *arr, short choice)
     break;
 
     case 2:
-        ReadFromFile(arr);
+        ReadFromFile(arr,length);
         break;
 
     case 3:
-        WriteToFile(arr, arraySize);
+        WriteToFile(arr, length);
         break;
 
     case 4:
@@ -88,32 +93,26 @@ contract *Choosing(contract *arr, short choice)
         int position;
         cin >> position;
         position -= 1;
-        (amount > arraySize) ? amount = arraySize : 0;
-        (position > arraySize) ? position = arraySize : 0;
+        (amount > length) ? amount = length : 0;
+        (position > length) ? position = length : 0;
 
-        return deleteElements(arr, arraySize, amount, position);
+        return deleteElements(arr, length, amount, position);
     }
     break;
 
     case 5:
-        ShowStruct(arr, arraySize);
+        ShowStruct(arr, length);
         break;
     case 6:
     {
-        cout << "\n1 цене \n2 по количеству: ";
+        cout << "\nСтоимость (0), количеству концертов(1), опыту(2): ";
         int sort;
         cin >> sort;
-        cout << "по возрастанию(1) убыванию(0): ";
-        bool sortingMethod;
-        cin >> sortingMethod;
-        for (int i = 0; i < arraySize; i++)
-            for (short j = 0; j < arraySize; j++)
-            {
-                if (sorting(arr[i].price, arr[j].price, sortingMethod) && sort)
-                    swap(arr[i], arr[j]);
-                if (sorting(arr[i].amount, arr[j].amount, sortingMethod) && sort == 2)
-                    swap(arr[i], arr[j]);
-            }
+        cout << "по возрастанию(1), убыванию(0): ";
+        bool sortingDirection;
+        cin >> sortingDirection;
+        sorting(arr,length ,sort,sortingDirection);
+
     }
     break;
 
@@ -122,8 +121,8 @@ contract *Choosing(contract *arr, short choice)
         cout << "\nВведите интересуемую строку: ";
         string index;
         cin >> index;
-        contract *buffArray = copyArray(arr, arraySize);
-        int buffArraySize = arraySize;
+        contract *buffArray = copyArray(arr, length);
+        int buffArraySize = length;
 
         for (int i = 0; i < buffArraySize; i++)
         {
@@ -142,6 +141,9 @@ contract *Choosing(contract *arr, short choice)
             WriteToFile(buffArray, buffArraySize);
         break;
     }
+    case 8:
+        findValue(arr,length);
+        break;
 
     default:
         break;
@@ -149,32 +151,98 @@ contract *Choosing(contract *arr, short choice)
     return arr;
 }
 
-void ShowStruct(contract *arr, int arraySize)
+void findValue(contract *arr,size_t length)
 {
-    cout << "size: " << arraySize;
-    for (short i = 0; i < arraySize; i++)
+    sorting(arr,length,2,0);
+    ShowStruct(arr,length);
+    cout<<"element to find: ";
+    int key;
+    cin>>key;
+    cout<<"способ поиска: (0)интерполяционный (1) бинарный";
+    bool algorthim;
+    cin>>algorthim;
+    int found;
+    if (algorthim)
+        found=binarySeacrh(arr,length,key);    
+    else found=InterpolSearch(arr,length,key)+1;
+    if (found==-1)
+        cout<<"значение не найдено\n";
+    else
+    cout<<"значение: "<<found;
+    
+}
+
+int InterpolSearch(contract *arr,size_t length,int key)
+{
+    int mid, left=0,right=length-1;
+    while (arr[left].amount<=key&&arr[right].amount>=key)
     {
-        cout << "\n\t\t\t"
-             << i + 1 << " контракт\n";
-        cout << "\tИмя\t"
-             << arr[i].name;
-        cout << "\n\tСтоимость\t" << arr[i].price;
-        cout << "\n\tКонцерты\t" << arr[i].amount;
-        cout << "\n\tОпыт\t" << arr[i].workExp;
+
+        mid=left+((key-arr[left].amount)*(right-left))/(arr[right].amount-arr[left].amount);
+        if (arr[mid].amount<key)
+            left=mid+1;
+            else if(arr[mid].amount>key)
+            right=mid-1;
+            else
+            return mid;
+    }
+    if(arr[left].amount==key)
+    return left;
+    else return -1;
+}
+
+int binarySeacrh(contract *arr, size_t length, int key)
+{
+    bool flag = false;
+    int left = 0, right = length - 1, mid;
+    while ((left <= right) && (flag != true))
+    {
+        mid = (left + right) / 2;
+        if (arr[mid].amount == key)
+            flag = true;
+        arr[mid].amount > key ? right = mid - 1 : left = mid + 1;
+    }
+
+    return (flag ? mid : -1);
+}
+
+void sorting(contract* arr,size_t length , int sortingValue,bool sortingDirection)
+{
+    for (int i = 0; i < length; i++)
+    {
+        for (short j = 0; j < length; j++)
+        {
+            if (!sortingValue && compare(arr[i].price, arr[j].price, sortingDirection))
+                swap(arr[i], arr[j]);
+            else if (sortingValue && compare(arr[i].amount, arr[j].amount, sortingDirection) )
+                swap(arr[i], arr[j]);
+            else if (!sortingValue==2 && compare(arr[i].workExp, arr[j].workExp, sortingDirection) )
+                swap(arr[i], arr[j]);
+        }
     }
 }
 
-contract *deleteElements(contract *array, int arraySize, int amount, int position)
+void ShowStruct(contract *arr, size_t length)
 {
-    contract *newArray = new contract[arraySize - amount];
+    cout << "size: " << length;
+    cout<<"\n№\tИмя \t\tСтоимость \tКоличество\tОпыт\n";
+    for (short i = 0; i < length; i++)
+    {
+        cout <<i + 1 <<"\t"<< arr[i].name<<"\t\t"<< arr[i].price<<"\t\t" <<arr[i].amount<<"\t\t"<<arr[i].workExp<<'\n';
+    }
+}
+
+contract *deleteElements(contract *array, size_t length, int amount, int position)
+{
+    contract *newArray = new contract[length - amount];
 
     for (short i = 0; i < position; i++)
         newArray[i] = array[i];
 
-    for (short i = position + amount, j = position; i < arraySize; i++, j++)
+    for (short i = position + amount, j = position; i < length; i++, j++)
         newArray[j] = array[i];
 
-    arraySize -= amount;
+    length -= amount;
     delete[] array;
 
     return newArray;
@@ -192,7 +260,7 @@ void FullContract(contract *cont)
     cin >> cont->workExp;
 }
 
-void ReadFromFile(contract *cont)
+void ReadFromFile(contract *cont,size_t length)
 {
     ifstream file;
     string line;
@@ -208,13 +276,12 @@ void ReadFromFile(contract *cont)
         return;
     }
 
-    for (short i = 0; i < arraySize; i++)
+    for (short i = 0; i < length; i++)
     {
-
-        short startName = line.find("NAME:") + 5;
-        short startPrice = line.find("PRICE:", startName) + 6;
-        short startAmount = line.find("AMOUNT:", startPrice) + 7;
-        short startExperience = line.find("EXPERIENCE:", startAmount) + 11;
+        int startName = line.find("NAME:") + 5;
+        int startPrice = line.find("PRICE:", startName) + 6;
+        int startAmount = line.find("AMOUNT:", startPrice) + 7;
+        int startExperience = line.find("EXPERIENCE:", startAmount) + 11;
 
         short npos = static_cast<short>(string::npos);
         if (startName == npos || startPrice == npos || startAmount == npos || startExperience == npos)
@@ -231,7 +298,7 @@ void ReadFromFile(contract *cont)
     }
 }
 
-void WriteToFile(contract *arr, int arraySize)
+void WriteToFile(contract *arr, size_t length)
 {
     ofstream file;
     string name;
@@ -243,14 +310,15 @@ void WriteToFile(contract *arr, int arraySize)
         cout << "wrong file";
         return;
     }
-    for (short i = 0; i < arraySize; i++)
+    for (short i = 0; i < length; i++)
         file << "NAME:" << arr[i].name << " PRICE:" << arr[i].price << " AMOUNT:" << arr[i].amount << " EXPERIENCE:" << arr[i].workExp << '\n';
 }
 
-contract *copyArray(contract *arr, int length)
+contract *copyArray(contract *arr, size_t length)
 {
     contract *newArray = new contract[length];
     for (int i = 0; i < length; i++)
         newArray[i] = arr[i];
     return newArray;
 }
+
